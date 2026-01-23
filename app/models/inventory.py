5e56ec2f50904274b6774fdf7bb1f5bdb84dc170
danlaratta <dan.laratta.jr@gmail.com>
2026-01-23 15:37:31 -0500

    Model - Inventory
    inventory model

---
+++ app/models/inventory.py
@@ -0,0 +1,24 @@
+from sqlalchemy import Integer, Date, func, String, Enum, Boolean, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from datetime import date
+from app.models.base import Base
+
+
+class Inventory(Base):
+    __tablename__ = 'inventories'
+
+    # Columns
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    quantity_in_stock: Mapped[int] = mapped_column(Integer, nullable=False)
+    received_at: Mapped[date] = mapped_column(Date, nullable=False, default=date.today)
+    expires_at: Mapped[date] = mapped_column(Date, nullable=False)
+
+
+    # Foriegn Key
+    # product id
+    # purchase order item id
+
+    # Relationships
+    # product 
+    # purchase order item 
+    # inventory log
