8c932ceb3d64c2698f209581e8930dc3ae5811bb
danlaratta <dan.laratta.jr@gmail.com>
2026-01-30 14:15:42 -0500

    Crud and Service - Delivery and PurchaseOrderItem
    added get crud method for PurchaseOrderItem and fixed create service method for Delivery removing relationship to item

--- app/crud/purchase_order_item_crud.py
+++ app/crud/purchase_order_item_crud.py
@@ -1,7 +1,7 @@
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.exc import IntegrityError
-
+from app.exceptions.database_exception import DatabaseException
+from app.models.purchase_order_item import PurchaseOrderItem
 
 class PurchaseOrderItemCrud:
     def __init__(self, db_session: AsyncSession) -> None:
@@ -14,8 +14,13 @@ class PurchaseOrderItemCrud:
 
 
     # Get PurchaseOrderItem 
-    async def get_order_item(self) -> None:
-        pass
+    async def get_order_item(self, order_item_id: int) -> PurchaseOrderItem:
+        result = await self.db_session.execute(select(PurchaseOrderItem).where(PurchaseOrderItem.id == order_item_id))
+        order_item: PurchaseOrderItem | None = result.scalar_one_or_none()
+
+        if order_item is None:
+            raise DatabaseException(f'No purchase order item found with id: {order_item_id}')
+        return order_item
 
 
     # Update PurchaseOrderItem 
--- app/services/delivery_services.py
+++ app/services/delivery_services.py
@@ -1,26 +1,27 @@
 from datetime import date, timedelta
 from app.crud.delivery_crud import DeliveryCrud
-from app.crud.item_crud import ItemCrud
+from app.crud.purchase_order_item_crud import PurchaseOrderItemCrud
 from app.enums.category_expiration import CategoryExpirationDays
 from app.models.delivery import Delivery
-from app.models.item import Item
+from app.models.purchase_order_item import PurchaseOrderItem
+
 
 class DeliveryService:
-    def __init__(self, delivery_crud: DeliveryCrud, item_crud: ItemCrud) -> None:
+    def __init__(self, delivery_crud: DeliveryCrud, order_item_crud: PurchaseOrderItemCrud) -> None:
         self.delivery_crud = delivery_crud
-        self.item_crud = item_crud
+        self.order_item_crud = order_item_crud
 
 
     # Create new delivery
-    async def create_new_delivery(self, quantity: int, received_at: date | None, item_id: int, purchase_order_item_id: int) -> Delivery:
-        # Get the item in delivery
-        item: Item = await self.item_crud.get_item(item_id)
+    async def create_new_delivery(self, quantity: int, received_at: date | None, purchase_order_item_id: int) -> Delivery:
+        # Get the purchase order item in delivery
+        order_item: PurchaseOrderItem = await self.order_item_crud.get_order_item(purchase_order_item_id)
     
         # Set received_at (defaults to today if no date provided)
         received_at = received_at or date.today()
 
         # Calculate the expiration date based on the item's category (None is for disposable items that don't expire)
-        expiration_days = CategoryExpirationDays[item.category.name].value
+        expiration_days = CategoryExpirationDays[order_item.item.category.name].value
         expires_at: date | None = None if expiration_days is None else received_at + timedelta(days=expiration_days)
 
         # Create delivery
@@ -28,8 +29,7 @@ class DeliveryService:
             quantity_in_delivery = quantity, 
             received_at = received_at,
             expires_at = expires_at,
-            item_id = item_id,
-            purchase_order_item_id = purchase_order_item_id
+            purchase_order_item_id = order_item.id
         )
         
         return await self.delivery_crud.create_delivery(delivery)
